8b9762168fa664e3f0c0a7fe8536a9efaaf59db8
Anastasiia Boico <167498327+Annieboic@users.noreply.github.com>
2025-10-07 11:33:22 -0500

    function handleMove added

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Board from "./Board";
 
 
 const Game = () => {
+
+    const initialState = new Array(9).fill(null)
+
+    const [board, setBoard] = useState(initialState);
+    const [player, setPlayer] = useState('X')
+
+
+    function handleMove(index) {
+        const newBoard = board.map((el, ind) => ind === index ? player : el)
+        setBoard(newBoard)
+        setPlayer(
+            player === 'X' ? 'O' : 'X'
+        )
+    }
+
+
+
 
 
 
     return (
         <div>
+            <Board
+                board={board}
+                handleMove={handleMove}
+            />
 
-            
+            <div>Next Player: {player}</div>
         </div>
     );
 };
